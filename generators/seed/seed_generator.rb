515b0c0707ec685f8ce90ffbc0c06a0aa241d58b
Patrick Reagan <patrick.reagan@viget.com>
2010-01-08 22:05:05 -0500

    Remove reference to non-existent YAML file

--- generators/seed/seed_generator.rb
+++ generators/seed/seed_generator.rb
@@ -2,7 +2,6 @@ class SeedGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       m.directory 'db/seeds'
-      m.file 'seed_order.yaml.example', "db/seeds/seed_order.yaml", :collision => :skip
 
       timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
       base_name = "#{timestamp}_#{name.underscore}"
